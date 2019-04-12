90f434d55b82126b0d724224d5a904f89e7a1e5f
Jason Castillo <jasson_72@hotmail.com>
2019-04-12 09:16:35 -0500

    Ejemplo de presentación de datos en pantalla

--- Ejemplo021/src/ejemplo021/Ejemplo021.java
+++ Ejemplo021/src/ejemplo021/Ejemplo021.java
@@ -13,9 +13,20 @@ public class Ejemplo021 {
 
     /**
      * @param args the command line arguments
+     *
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // este método es el principla de mi clase
+        String nombrePersona; // es la variable para almacenar un nombre
+        String apellidoPersona;
+
+        nombrePersona = "Jason José";
+        apellidoPersona = "Castillo Pulla";
+
+        System.out.println("Mi nombre es: " + nombrePersona+"\n\n\tMi apellido "
+                + "es: "+ apellidoPersona);
+
+
     }
-    
+
 }
